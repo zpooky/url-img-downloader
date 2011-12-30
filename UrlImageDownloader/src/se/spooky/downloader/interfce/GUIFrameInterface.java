e5965999fd880e430d310a2789c1e3b5e0bbe7a1
Fredrik Olsson <hell_trooper@msn.com>
2011-12-30 15:28:46 +0100

    .

--- UrlImageDownloader/src/se/spooky/downloader/interfce/GUIFrameInterface.java
+++
@@ -1,9 +0,0 @@
-package se.spooky.downloader.interfce;
-
-import se.spooky.downloader.url.Direction;
-
-public interface GUIFrameInterface {
-	public void addLogMessage(String message);
-
-	public void setFileReceiveDone(Direction direction);
-}
--- UrlImageDownloader/src/se/spooky/downloader/interfce/GUIFrameMainPanelInterface.java
+++
@@ -1,7 +0,0 @@
-package se.spooky.downloader.interfce;
-
-import java.net.URL;
-
-public interface GUIFrameMainPanelInterface {
-	public void onStartButton(URL url);
-}
